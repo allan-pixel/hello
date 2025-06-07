68a01ec5b1910f85dd2f4acddaf93804b99dd9c8
allan-pixel <136108720+allan-pixel@users.noreply.github.com>
2025-06-07 10:45:14 -0300

    Add files via upload

---
+++ leitura.py
@@ -0,0 +1,11 @@
+def main():
+    nome = input("Digite seu nome: ")
+    idade = int(input("Digite sua idade"))
+    altura = float(input("Digite sua altura"))
+    isMasculino = input("Você é do sexo masculino? (True/False)")
+    print("Nome:", nome)
+    print("Idade", idade)
+    print("Altura", altura)
+    print("É do sexo masculino?", isMasculino)
+    return 0
+main()
